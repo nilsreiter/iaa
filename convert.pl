59f63209b030fbea4e60713f4c86110dcd045b03
Nils Reiter <nils.reiter@ims.uni-stuttgart.de>
2015-04-08 12:09:24 +0200

    Number of classes is counted from data

--- convert.pl
+++ convert.pl
@@ -7,26 +7,41 @@
 # 
 # Variables
 #   R: The number of raters. If a class got more than R raters, 
-       R will be printed instead.
+#      R will be printed instead.
 #   C: The number of classes.
+#
+# Changelog
+# 2.0
+#    Number of classes is counted from data. Can still be overwritten,
+#    if some categories never appear.
+# 1.0
+#    Initial release
 
 use warnings;
 use Getopt::Long;
 
+
 my %data = ();
 my $raters = 5;
-my $classes = 3;
+my $classes = undef;
 
 GetOptions(
 	"raters=i" => \$raters,
 	"classes=i" => \$classes
 ) or die("Error in command line arguments\n");
 
+my %cl = ();
+
 while(<>) {
 	chomp;
 	my @line = split;
 	$data{$line[1]}{$line[2]} = ($line[0]>$raters?$raters:$line[0]);
+	$cl{$line[2]} = 1;
 }
+
+if (!defined $classes) {
+	$classes = scalar(keys %cl);
+};
 
 foreach my $key (keys %data) {
 	#print $key;
